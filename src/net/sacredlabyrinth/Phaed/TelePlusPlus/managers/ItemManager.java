4248fbfd3bfc1683dffd7e2f492cb5e60869fb1e
Lycano <luricos@gmx.de>
2011-04-14 03:18:09 +0200

    * changed tab policy to spaces only

--- src/net/sacredlabyrinth/Phaed/TelePlusPlus/managers/ItemManager.java
+++ src/net/sacredlabyrinth/Phaed/TelePlusPlus/managers/ItemManager.java
@@ -31,42 +31,42 @@ public class ItemManager
     
     public boolean PutItemInHand(Player player, Material item)
     {
-		ItemStack handitem = player.getItemInHand();
-		Inventory inv = player.getInventory();
-		
-		if (!handitem.getType().equals(item))
-		{
-			if (!handitem.getType().equals(Material.AIR))
-			{
-			    if(inv.firstEmpty() == -1)
-			    {
-			    	player.sendMessage(ChatColor.RED + "No space in your inventory");
-			    	return false;
-			    }
-			    
-			    inv.setItem(inv.firstEmpty(), handitem);
-			}
-			
-			if (inv.contains(item))
-			{
-				Integer slotId = inv.first(item);
-				ItemStack stack = inv.getItem(slotId);
-				Integer stackAmount = stack.getAmount();
-				
-		    	if (stackAmount > 1 )
-		    	{ 
-		    		stack.setAmount( stackAmount - 1 );
-	    		} else { 
-	    			inv.clear(slotId);
-    			}
-			}
-			
-			player.setItemInHand(new ItemStack(item, 1));
-		} else {
-			player.sendMessage(ChatColor.RED + "Can't you feel it? You already have this item in hand");
-			return false;
-		}
-		
-		return true;
+        ItemStack handitem = player.getItemInHand();
+        Inventory inv = player.getInventory();
+        
+        if (!handitem.getType().equals(item))
+        {
+            if (!handitem.getType().equals(Material.AIR))
+            {
+                if(inv.firstEmpty() == -1)
+                {
+                    player.sendMessage(ChatColor.RED + "No space in your inventory");
+                    return false;
+                }
+                    
+                inv.setItem(inv.firstEmpty(), handitem);
+            }
+        
+        if (inv.contains(item))
+        {
+            Integer slotId = inv.first(item);
+            ItemStack stack = inv.getItem(slotId);
+            Integer stackAmount = stack.getAmount();
+        
+            if (stackAmount > 1 )
+            { 
+                stack.setAmount( stackAmount - 1 );
+            } else { 
+                inv.clear(slotId);
+            }
+        }
+        
+            player.setItemInHand(new ItemStack(item, 1));
+        } else {
+            player.sendMessage(ChatColor.RED + "Can't you feel it? You already have this item in hand");
+            return false;
+        }
+        	
+        return true;
     }
 }
